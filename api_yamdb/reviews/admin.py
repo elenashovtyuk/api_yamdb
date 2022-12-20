70e39c1dabf269d3815c8098e25501fb99d6af84
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2022-12-21 00:35:15 +0300

    register models Title, Category, Genre

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Title, Category, Genre
+
+# Чтобы добавить эти модели в интерфейс администратора,
+# их надо зарегистрировать в файле reviews/admin.py.
+
+# В файле reviews/admin.py создаем классы
+# TitleAdmin, CategoryAdmin, GenreAdmin,
+# наследующиеся от admin.ModelAdmin,
+# и зарегистрируем их как источники конфигурации для моделей
+# Title, Category, Genre
+
+
+class TitleAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', 'name', 'year', 'description', 'category',)
+    # Добавляем интерфейс для поиска по произведениям
+    search_fields = ('name',)
+    # Добавляем возможность фильтрации названию
+    list_filter = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', 'name', 'slug',)
+    # добавляем интерфейс для поиска по категориям
+    search_fields = ('name',)
+    # добавляем возможность фильтрации по названию
+    list_filter = ('name',)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', 'name', 'slug',)
+    # добавляем интерфейс для поиска по жанрам
+    search_fields = ('name',)
+    # добавляем возможность фильтрации по названию
+    list_filter = ('name',)
+
+
+# При регистрации модели Title источником конфигурации для неё назначаем
+# класс TitleAdmin
+admin.site.register(Title, TitleAdmin)
+# При регистрации модели Category источником конфигурации для неё назначаем
+# класс CategoryAdmin
+admin.site.register(Category, CategoryAdmin)
+# При регистрации модели Genre источником конфигурации для неё назначаем
+# класс GenreAdmin
+admin.site.register(Genre, GenreAdmin)

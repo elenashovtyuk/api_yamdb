136c6f01a80c39811c9c0f9cce39909921c9133f
Мария Лапикова <mashik_p@mail.ru>
2022-12-24 14:16:44 +0300

    delete extra files; modify serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,16 +1,37 @@
+from rest_framework.relations import SlugRelatedField
 from reviews.models import Review, Comment
 
 from rest_framework import serializers
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    # author = SlugRelatedField(read_only=True, slug_field='username')
+    author = SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    title = SlugRelatedField(read_only=True, slug_field='name')
 
     class Meta:
         fields = '__all__'
         model = Review
+        # validators = [
+        #     UniqueTogetherValidator(
+        #         queryset=Review.objects.all(),
+        #         fields=('author', 'title')
+        #     )
+        # ]
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    title = SlugRelatedField(read_only=True, slug_field='name')
+    review = SlugRelatedField(read_only=True, slug_field='text')
 
     class Meta:
         fields = '__all__'

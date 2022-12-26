1a03caaf8dfe6f4e44ce746aac6475bb8f69c18d
Мария Лапикова <mashik_p@mail.ru>
2022-12-26 05:42:57 +0300

    update admin part of my model objects

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -4,15 +4,16 @@ from .models import Review, Comment
 
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'title', 'author', 'score',)
-    search_fields = ('text', 'author', 'title',)
-    list_filter = ('title', 'author',)
+    list_display = ('pk', 'text', 'author', 'title', 'score',)
+    search_fields = ('text', 'author', 'title')
+    list_filter = ('title', 'author', 'score')
+    ordering = ('-score',)
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'title', 'author')
-    search_fields = ('text', 'author', 'title',)
-    list_filter = ('title', 'author',)
+    list_display = ('pk', 'text', 'review', 'title', 'author',)
+    search_fields = ('text', 'author', 'review')
+    list_filter = ('title', 'author', 'review')
 
 
 admin.site.register(Review, ReviewAdmin)

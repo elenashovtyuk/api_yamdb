ae2cc3409f3ac764eedcdc1709727d4907ac49e1
Мария Лапикова <mashik_p@mail.ru>
2022-12-27 16:56:15 +0300

    added docstring to user validator

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_me(value):
+    """Проверка равенства значения строке 'me'."""
     if value == 'me':
         raise ValidationError(
             'Использовать имя me в качестве username запрещено.'

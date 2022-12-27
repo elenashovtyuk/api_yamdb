fb718f808622aec3d238094eae14b5cf0461c3e2
Мария Лапикова <mashik_p@mail.ru>
2022-12-27 16:52:25 +0300

    beautified validator for user according to pep8

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -2,9 +2,8 @@ from django.core.exceptions import ValidationError
 
 
 def validate_me(value):
-    if value == "me":
+    if value == 'me':
         raise ValidationError(
-            "Использовать имя 'me' в качестве username запрещено."
+            'Использовать имя me в качестве username запрещено.'
         )
-
     return value

7d8d369b22180e6f184bbb9795ce1c629df98d73
Мария Лапикова <mashik_p@mail.ru>
2022-12-27 16:58:06 +0300

    added docstring to admin reviews

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -4,34 +4,41 @@ from .models import Title, Category, Genre, Review, Comment
 
 
 class TitleAdmin(admin.ModelAdmin):
+    """Класс отображения параметров Title в административной части."""
     list_display = ('pk', 'name', 'year', 'description', 'category',)
     search_fields = ('name',)
     list_filter = ('name',)
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    """Класс отображения параметров Category в административной
+    части. """
     list_display = ('pk', 'name', 'slug',)
     search_fields = ('name',)
     list_filter = ('name',)
 
 
 class GenreAdmin(admin.ModelAdmin):
+    """Класс отображения параметров Genre в административной части."""
     list_display = ('pk', 'name', 'slug',)
     search_fields = ('name',)
     list_filter = ('name',)
     
 
 class ReviewAdmin(admin.ModelAdmin):
+    """Класс отображения параметров Review в административной части."""
     list_display = ('pk', 'text', 'author', 'title', 'score',)
-    search_fields = ('text', 'author', 'title')
-    list_filter = ('title', 'author', 'score')
+    search_fields = ('text', 'author', 'title',)
+    list_filter = ('title', 'author', 'score',)
     ordering = ('-score',)
 
 
 class CommentAdmin(admin.ModelAdmin):
+    """Класс отображения параметров Comments в административной
+    части. """
     list_display = ('pk', 'text', 'review', 'title', 'author',)
-    search_fields = ('text', 'author', 'review')
-    list_filter = ('title', 'author', 'review')
+    search_fields = ('text', 'author', 'review',)
+    list_filter = ('title', 'author', 'review',)
 
 
 admin.site.register(Title, TitleAdmin)

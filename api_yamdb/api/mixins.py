501dec50a041d7c5c10f0fcfd2bd0057461aa86c
Мария Лапикова <mashik_p@mail.ru>
2022-12-27 17:07:24 +0300

    added docstring to api mixins

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -3,4 +3,5 @@ from rest_framework import mixins, viewsets
 
 class CLDViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
                  mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    """Класс-родитель для представлений категорий и жанров."""
     pass

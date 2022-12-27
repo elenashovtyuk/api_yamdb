9a17155a996e6d122e0bf4a994f1d2fe160e9602
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2022-12-27 12:10:48 +0300

    delete mixins from reviews

--- api_yamdb/reviews/mixins.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import mixins, viewsets
-
-
-class CLDViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
-                 mixins.DestroyModelMixin, viewsets.GenericViewSets):
-    pass

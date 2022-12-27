10b96c29796de32f028edc7a4bfc5aecb8b7abdf
Мария Лапикова <mashik_p@mail.ru>
2022-12-27 17:06:16 +0300

    added docstring to api filters

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,13 +1,9 @@
 from django_filters import rest_framework as filters
 from reviews.models import Title
 
-# создадим свой класс-фильтр на основе FilterSet
-# класса, который позволяет определить - как фильтровать различные поля модели
-# name указывает, поле, которое нужно фильтровать
-# lookup_expr
-
 
 class FilterForTitle(filters.FilterSet):
+    """Описание фильтрации по полям модели Title для ViewSet."""
     name = filters.CharFilter(field_name='name')
     category = filters.CharFilter(field_name='category__slug')
     genre = filters.CharFilter(field_name='genre__slug')

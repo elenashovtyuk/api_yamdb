49c78c51f0c6b9bd1cef41000790b208b6d9c2cd
Мария Лапикова <mashik_p@mail.ru>
2022-12-27 16:50:49 +0300

    beautified user model according to pep8

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,57 +1,65 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.core.validators import RegexValidator
+from django.db import models
+
 from .validators import validate_me
 
-
 USER_ROLES = (
-    ("user", "пользователь"),
-    ("moderator", "модератор"),
-    ("admin", "администратор"),
-    )
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+)
+
 
 class User(AbstractUser):
     username = models.CharField(
+        verbose_name='Имя пользователя',
         max_length=150,
         unique=True,
-        validators=(RegexValidator(regex=r'^[\w.@+-]+\Z',  message= 'недопустимое имя пользователя',),
-        validate_me)
+        validators=(
+            RegexValidator(
+                regex=r'^[\w.@+-]+\Z',
+                message='Недопустимое имя пользователя'
+            ),
+            validate_me,)
         )
-    email = models.EmailField(verbose_name="Электронная почта", max_length=254, unique=True)
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        max_length=254,
+        unique=True
+    )
     bio = models.TextField(
-        verbose_name="Биография пользователя",
+        verbose_name='Биография пользователя',
         blank=True,
         null=True,
     )
     confirmation_code = models.CharField(
-        verbose_name="Код подтверждения", max_length=6, default="000000"
+        verbose_name='Код подтверждения', max_length=6, default='000000'
     )
     role = models.CharField(
-        verbose_name="Роль пользователя",
-        max_length=9,
+        verbose_name='Роль пользователя',
+        max_length=13,
         choices=USER_ROLES,
-        default="user",
         blank=True,
         null=True,
+        default='user'
     )
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ("username")
 
-    objects =  UserManager()
+    REQUIRED_FIELDS = ('email',)
+
+    objects = UserManager()
 
     @property
     def is_admin(self):
-        return self.role == "admin"
+        return self.role == 'admin'
 
     @property
     def is_moderator(self):
-        return self.role == "moderator"
+        return self.role == 'moderator'
 
     @property
     def is_user(self):
-        return self.role == "user"
-
-
+        return self.role == 'user'
 
     def __str__(self):
         return self.email
